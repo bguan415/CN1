21566d32a3620899a3ed6b21d25b6ac4adb4d3f6
CG-DAVIS <60015986+CG-DAVIS@users.noreply.github.com>
2021-02-17 16:51:48 -0800

    Update UI.java

--- UI.java
+++ UI.java
@@ -1,1 +1,16 @@
 
+// Main UI for the Timer App
+
+// Timer Display Screen
+// Timers are sectioned boxes in a list. Each box has a variety of labels and buttons.
+// On the left of the timer box is the size section. This is a combination button that iterates the size, and a label that displays the size.
+// On the right is a the timer section. This displays the current amount of time passed; tapping it starts/stops the timer.
+//    (Only show two most relevant times here? Minutes/seconds, hours/minutes, days/hours?) (Change color for start/stop?)
+// The middle section contains the name label. Tapping it opens the timer box (Accordion funtion? See CN1) showing the description.
+// From the "opened" view, the user can tap the name or description to edit them. The full time passed could also be shown on the right?
+// An X button also appears by the name when a timer is opened, which allows the user to delete.
+
+// At the bottom of the timer scroll screen, there are two buttons. New Timer adds a timer, Statistics opens the Statistics Screen
+
+// Statistics Screen 
+// Four buttons to show stats by size? A search bar to find a specific timer? A log? Settings?
