3f4984897d9902b460f851f792bf9c5a659f9830
davidjimUCD <44990369+davidjimUCD@users.noreply.github.com>
2021-02-17 16:28:43 -0800

    Add task class
    Add task class

--- Data.java
+++ Data.java
@@ -10,3 +10,30 @@ public class Data {
 
 
 }
+
+class Task {
+    public Task(String taskName) {
+        this.taskName = taskName;
+        size = "";
+        descriptions = new ArrayList<>();
+        timeSpentOnTask = new HashMap<String,Integer>();
+        timeSpentOnTask.put("hours", 0);
+        timeSpentOnTask.put("minutes", 0);
+        timeSpentOnTask.put("seconds", 0);
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    
+    public void incrementRunTime(String startTime, String stopTime) {
+    private String taskName;
+    String size;
+    private Map<String, Integer> timeSpentOnTask;
+    private List<String> descriptions;
+}
