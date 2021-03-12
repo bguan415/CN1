c77de6cee3274d084e3e1fdd92a50ee0618466d5
CG-DAVIS <60015986+CG-DAVIS@users.noreply.github.com>
2021-03-11 23:20:37 -0800

    Update UI.java

--- UI.java
+++ UI.java
@@ -1,6 +1,8 @@
 package org.ecs160.a2;
 
 import com.codename1.components.FloatingActionButton;
+import com.codename1.components.MultiButton;
+import com.codename1.components.SpanButton;
 import com.codename1.ui.*;
 import com.codename1.ui.layouts.*;
 import com.codename1.ui.plaf.*;
@@ -31,17 +33,21 @@ public class UI extends Form {
 
     public Container taskList;
     private int taskNumber;
-    Data sqler = new Data();
 
     public UI() {
 
         logic = new Logic();
 
-
         setTitle("[APP NAME HERE]");
         setLayout(new BorderLayout());
 
-        Container taskList = createTaskList();
+        taskList = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        taskList.setScrollableY(true);
+        taskList.getAllStyles().setFgColor(0x6b7e8c);
+        taskList.getAllStyles().setBgTransparency(255);
+        taskList.getAllStyles().setBgColor(0x37454f);
+        taskList.getStyle().setPadding(20, 20, 20, 20);
+        taskList.getAllStyles().setBorder(Border.createLineBorder(6, 0x37454f));
 
         try {
             ResultSet rs = logic.GetAllTasks();
@@ -58,20 +64,11 @@ public class UI extends Form {
             System.out.println(e.getMessage());
         }
 
-        Container blankSpace = createSpacing();
-        createNewTaskButton();
-        createStatPageButton();
+        Container blankSpace = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        blankSpace.getAllStyles().setFgColor(0x99a1a1);
+        blankSpace.getAllStyles().setBgTransparency(255);
+        blankSpace.getAllStyles().setBgColor(0x37454f);
 
-        getToolbar().addSearchCommand(e -> search((String)e.getSource()));
-
-        setScrollableX(false);
-        setScrollableY(false);
-        add(BorderLayout.NORTH, taskList);
-        add(BorderLayout.CENTER, blankSpace);
-
-    }
-
-    void createNewTaskButton() {
         FloatingActionButton fab = FloatingActionButton.createFAB(FontImage.MATERIAL_ADD);
         fab.getAllStyles().setFgColor(0x2e3030);
         fab.getAllStyles().setBgColor(0x6b7e8c);
@@ -81,41 +78,20 @@ public class UI extends Form {
             Dialog popup = taskPopup();
             popup.show();
         });
-    }
 
-    void createStatPageButton() {
         FloatingActionButton fab2 = FloatingActionButton.createFAB(FontImage.MATERIAL_ASSESSMENT);
         fab2.getAllStyles().setFgColor(0x2e3030);
         fab2.getAllStyles().setBgColor(0x6b7e8c);
         fab2.getAllStyles().setBgTransparency(255);
         fab2.bindFabToContainer(getContentPane());
-        fab2.addActionListener(e -> {
-            if(sqler.CountAllTasks() > 0) {
-                new SummaryPage(logic).show();
-            } else {
-                Dialog popup = NoTasksPopup();
-                popup.show();
-            }
-        });
-    }
+        fab2.addActionListener(e -> new SummaryPage(logic).show());
 
-    Container createTaskList() {
-        taskList = new Container(new BoxLayout(BoxLayout.Y_AXIS));
-        taskList.setScrollableY(true);
-        taskList.getAllStyles().setFgColor(0x6b7e8c);
-        taskList.getAllStyles().setBgTransparency(255);
-        taskList.getAllStyles().setBgColor(0x37454f);
-        taskList.getStyle().setPadding(20, 20, 20, 20);
-        taskList.getAllStyles().setBorder(Border.createLineBorder(6, 0x37454f));
-        return taskList;
-    }
+        getToolbar().addSearchCommand(e -> search((String)e.getSource()));
 
-    Container createSpacing(){
-        Container blankSpace = new Container(new BoxLayout(BoxLayout.Y_AXIS));
-        blankSpace.getAllStyles().setFgColor(0x99a1a1);
-        blankSpace.getAllStyles().setBgTransparency(255);
-        blankSpace.getAllStyles().setBgColor(0x37454f);
-        return blankSpace;
+        setScrollableY(false);
+        add(BorderLayout.NORTH, taskList);
+        add(BorderLayout.CENTER, blankSpace);
+
     }
 
     void search(String text) {
@@ -152,7 +128,7 @@ public class UI extends Form {
         warning.getStyle().setFgColor(0xff0000);
 
         TextField nameBar = new TextField();
-        nameBar.setHint(setTaskHint());
+        nameBar.setHint(setFreeName());
 
         GridLayout gl = new GridLayout(1,2);
         Container buttons = new Container(gl);
@@ -191,61 +167,7 @@ public class UI extends Form {
         return popup;
     }
 
-    private Dialog NoTasksPopup() {
-        BorderLayout bl = new BorderLayout();
-        Dialog popup = new Dialog(bl);
-
-        TableLayout tl = new TableLayout(2,1);
-        Container instructions = new Container(tl);
-        Label instruction = new Label("No tasks saved. Enter a new task:");
-        instruction.getStyle().setAlignment(CENTER);
-        instructions.add(instruction);
-
-        Label warning = new Label("");
-        warning.getStyle().setAlignment(CENTER);
-        warning.getStyle().setFgColor(0xff0000);
-
-        TextField nameBar = new TextField();
-        nameBar.setHint(setTaskHint());
-
-        GridLayout gl = new GridLayout(1,2);
-        Container buttons = new Container(gl);
-        Button confirmButton = new Button("Confirm");
-        Button cancelButton = new Button("Cancel");
-        buttons.add(cancelButton);
-        buttons.add(confirmButton);
-
-        confirmButton.addActionListener(ev -> {
-            if (nameBar.getText() != "") {
-                if (logic.TaskExists(nameBar.getText())) {
-                    nameBar.setText("");
-                    warning.setText("That task name is already in use.");
-                    instructions.add(warning);
-                    popup.growOrShrink();
-                    //popup.animateLayout(5);
-                } else {
-                    taskList.add(addTask(nameBar.getText()));
-                    taskList.animateLayout(5);
-                    popup.dispose();
-                }
-            }
-            else {
-                taskList.add(addTask(nameBar.getHint()));
-                taskList.animateLayout(5);
-                popup.dispose();
-            }
-        });
-
-        cancelButton.addActionListener(ex -> popup.dispose());
-
-        popup.add(BorderLayout.NORTH, instructions);
-        popup.add(BorderLayout.CENTER, nameBar);
-        popup.add(BorderLayout.SOUTH, buttons);
-
-        return popup;
-    }
-
-    private String setTaskHint() {
+    private String setFreeName() {
         String setName = "";
         int taskGuess = 1;
         while (setName.equals("")) {
@@ -261,24 +183,12 @@ public class UI extends Form {
     public Task addTask(String name) {
         // Initialization for task viewer
         TableLayout tl = new TableLayout(1, 4);
-        String taskName;
-        if (name.equals("")) { taskName = "Task " + (taskNumber + 1); }
-        else { taskName = name; }
 
         Task upper = newStyledTask(tl, name);
 
-        // Initialization for size, name, and time button
+        // Initialization for size, name, and time buttons
         upper.add(tl.createConstraint().widthPercentage(20), upper.createSizeButton());
-        upper.add(tl.createConstraint().widthPercentage(60), upper.createSizeButton());
-
-        Accordion accr = new Accordion();
-        TextArea ta = new TextArea(7, 40);
-        accr.addContent(taskName, ta);
-        ta.setHint("Description");
-        upper.add(tl.createConstraint().widthPercentage(60), accr);
-        ta.getAllStyles().setBgTransparency(0);
-
-        // Init for Time
+        upper.add(tl.createConstraint().widthPercentage(60), upper.createAccordion());
         upper.add(tl.createConstraint().widthPercentage(-2), upper.createTimeButton());
 
         logic.CreateNewTask(name, "S");
@@ -290,22 +200,15 @@ public class UI extends Form {
         // Initialization for task viewer
         TableLayout tl = new TableLayout(1, 4);
         String taskName;
-        if (name.equals("")) { taskName = "Task " + (taskNumber + 1); }
+        if (name.equals("")) { taskName = setFreeName(); }
         else { taskName = name; }
 
         Task upper = newStyledTask1(tl, taskName, size, description, runTime);
-        // Initialization for size button
+
+        // Initialization for size, description, and time buttons
         upper.add(tl.createConstraint().widthPercentage(20), upper.createSizeButton(size));
-
-        Accordion accr = new Accordion();
-        TextArea ta = new TextArea(7, 40);
-        accr.addContent(taskName, ta);
-        ta.setHint(description == null ? "Description" : description);
-        upper.add(tl.createConstraint().widthPercentage(60), accr);
-        ta.getAllStyles().setBgTransparency(0);
-
-        // Init for Time
-        upper.add(tl.createConstraint().widthPercentage(-2), upper.createTimeButton(runTime));
+        upper.add(tl.createConstraint().widthPercentage(60), upper.createAccordion());
+        upper.add(tl.createConstraint().widthPercentage(-2), upper.createTimeButton());
 
         if(isNew) logic.CreateNewTask(taskName, size);
 
@@ -313,7 +216,7 @@ public class UI extends Form {
     }
 
     private Task newStyledTask(Layout tl, String name) {
-        Task upper = new Task(tl, name, logic);
+        Task upper = new Task(tl, name);
 
         Stroke borderStroke = new Stroke(2, Stroke.CAP_SQUARE, Stroke.JOIN_MITER, 1);
 
@@ -361,11 +264,14 @@ public class UI extends Form {
         int currentSize;
         String description;
         int runTime;
+        TimeDisplay timer;
 
-        public Task(Layout t1, String name, Logic log) {
+        public Task(Layout t1, String name) {
             super(t1);
             currentSize = 0;
             taskName = name;
+            description = "";
+            runTime = 0;
         }
 
         public Task(Layout t1, String name, String size, String description, int runTime) {
@@ -375,6 +281,7 @@ public class UI extends Form {
             this.description = description;
             this.runTime = runTime;
         }
+
 
         public Button createSizeButton() {
             Button size = new Button("S");
@@ -386,48 +293,6 @@ public class UI extends Form {
             size.addActionListener((e) -> changeSize(size));
 
             return size;
-        }
-
-        public Component createAccordion() {
-            Accordion accr = new Accordion();
-            BorderLayout bl = new BorderLayout();
-            Container body = new Container(bl);
-
-            TextArea ta = new TextArea(7, 40);
-            ta.setHint("Description");
-            ta.getAllStyles().setBgTransparency(100);
-
-            TableLayout tl = new TableLayout(1,2);
-            Container RenameBox = new Container(tl);
-            Button renamer = new Button("Rename");
-            TextField nameSet = new TextField();
-            RenameBox.add(tl.createConstraint().widthPercentage(55), nameSet);
-            RenameBox.add(tl.createConstraint().widthPercentage(-2), renamer);
-
-            body.add(BorderLayout.CENTER, ta);
-            body.add(BorderLayout.SOUTH, RenameBox);
-
-            accr.addContent(taskName, body);
-            accr.setScrollableY(false);
-
-            renamer.addActionListener((e) -> {
-                changeName(nameSet.getText());
-                accr.setHeader(taskName, body);
-            });
-            accr.addOnClickItemListener((e) -> sendDesc(ta));
-
-            return accr;
-        }
-
-        private void sendDesc(TextArea ta) {
-            logic.SetTaskDescription(taskName, ta.getText());
-        }
-
-        private void changeName(String newName) {
-            if (!logic.TaskExists(newName)) {
-                logic.RenameTask(taskName,newName);
-                taskName = newName;
-            }
         }
 
         public Button createSizeButton(String sizeStr) {
@@ -452,24 +317,14 @@ public class UI extends Form {
         }
 
         public UI.TimeDisplay createTimeButton() {
-            UI.TimeDisplay time = new UI.TimeDisplay();
+            UI.TimeDisplay time = new UI.TimeDisplay(runTime);
             time.getStyle().setAlignment(CENTER);
             time.getAllStyles().setFgColor(0x752c29);
             time.getAllStyles().setBgTransparency(0);
             time.getAllStyles().setBgColor(0x752c29);
 
             time.addActionListener((e) -> toggleRunning(time));
-            return time;
-        }
-
-        public UI.TimeDisplay createTimeButton(int runTime) {
-            UI.TimeDisplay time = new UI.TimeDisplay(logic.GenerateTimeStringFromSeconds(runTime));
-            time.getStyle().setAlignment(CENTER);
-            time.getAllStyles().setFgColor(0x752c29);
-            time.getAllStyles().setBgTransparency(0);
-            time.getAllStyles().setBgColor(0x752c29);
-
-            time.addActionListener((e) -> toggleRunning(time));
+            timer = time;
             return time;
         }
 
@@ -484,24 +339,73 @@ public class UI extends Form {
             }
         }
 
-        private int SizeStringtoInt(String size) {
-            switch(size) {
-                case "S":
-                    return 0;
-                case "M":
-                    return 1;
-                case "L":
-                    return 2;
-                case "XL":
-                    return 3;
-                default:
-                    return 0;
+        public Component createAccordion() {
+            Accordion accr = new Accordion();
+            BorderLayout bl = new BorderLayout();
+            Container body = new Container(bl);
 
+            TextArea ta = new TextArea(7, 40);
+            ta.setHint("Description");
+            ta.setText(description == null ? "" : description);
+            ta.getAllStyles().setBgTransparency(100);
+
+            TableLayout tl = new TableLayout(1,2);
+            Container RenameBox = new Container(tl);
+            Button renamer = new Button("Rename");
+            TextField nameSet = new TextField();
+            RenameBox.add(tl.createConstraint().widthPercentage(55), nameSet);
+            RenameBox.add(tl.createConstraint().widthPercentage(-2), renamer);
+
+            Label vText = new Label("SampleText");
+
+            body.add(BorderLayout.NORTH, vText);
+            body.add(BorderLayout.CENTER, ta);
+            body.add(BorderLayout.SOUTH, RenameBox);
+
+            accr.addContent(taskName, body);
+            accr.setScrollableY(false);
+
+            renamer.addActionListener((e) -> {
+                changeName(nameSet.getText());
+                accr.setHeader(taskName, body);
+            });
+            accr.addOnClickItemListener((e) -> {
+                sendDesc(ta);
+                vText.setText(timer.getVerbose());
+            });
+
+            return accr;
+        }
+
+        private void changeName(String newName) {
+            if (!logic.TaskExists(newName)) {
+                logic.RenameTask(taskName,newName);
+                taskName = newName;
+            }
+        }
+
+        private void sendDesc(TextArea ta) {
+            logic.SetTaskDescription(taskName, ta.getText());
+        }
+
+        private int SizeStringtoInt(String size) {
+            if(size.equals("S")) {
+                return 0;
+            }
+            if(size.equals("M")) {
+                return 1;
+            }
+            if(size.equals("L")) {
+                return 2;
+            }
+            if(size.equals("XL")) {
+                return 3;
+            }
+            else {
+                return 0;
             }
         }
     }
-
-
 
     public class TimeDisplay extends Button {
         int seconds;
@@ -511,25 +415,16 @@ public class UI extends Form {
         public String timeStamp;
         int lastSecond;
         boolean timerRunning;
+        String verbose;
 
-        public TimeDisplay() {
-            seconds = 0;
-            minutes = 0;
-            hours = 0;
-            days = 0;
-            timeStamp = "00:00";
-            Calendar curTime = Calendar.getInstance();
-            lastSecond = curTime.get(Calendar.SECOND);
-            timerRunning =  false;
-            setText(timeStamp);
-        }
-
-        public TimeDisplay(String runTime) {
-            seconds = 0;
-            minutes = 0;
-            hours = 0;
-            days = 0;
-            timeStamp = runTime;
+        public TimeDisplay(int timing) {
+            verbose = "";
+            seconds = (timing%3600)%60;
+            minutes = (timing%3600)/60;
+            hours = (timing%3600);
+            days = hours/24;
+            hours = hours-(days*24);
+            setTimeString();
             Calendar curTime = Calendar.getInstance();
             lastSecond = curTime.get(Calendar.SECOND);
             timerRunning =  false;
@@ -556,6 +451,8 @@ public class UI extends Form {
             return false;
         }
 
+        public String getVerbose() { return verbose; }
+
         private boolean timePassed() {
             Calendar curTime = Calendar.getInstance();
             int  curSec = curTime.get(Calendar.SECOND);
@@ -580,6 +477,7 @@ public class UI extends Form {
                 hours = 0;
                 days++;
             }
+            verboseString();
             setTimeString();
         }
 
@@ -594,6 +492,14 @@ public class UI extends Form {
             else {
                 timeStamp = pad(minutes) + ":" + pad(seconds);
             }
+        }
+
+        private void verboseString() {
+            String sDays = "D: " + pad(days);
+            String sHours = " - H: " + pad(hours);
+            String sMins = " - M: " + pad(minutes);
+            String sSecs = " - S: " + pad(seconds);
+            timeStamp = sDays + sHours + sMins + sSecs;
         }
 
         private String pad(int number) {
