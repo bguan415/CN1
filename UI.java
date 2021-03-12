d72b42ccf386847744ef110fc458e56ec762fe28
davidjimUCD <44990369+davidjimUCD@users.noreply.github.com>
2021-03-12 13:12:48 -0800

    Separates TimeDisplay into new file
    TimeDisplay class is now its ints own java file

--- UI.java
+++ UI.java
@@ -351,8 +351,8 @@ public class UI extends Form {
             logic.ResizeTask(taskName,sizes[currentSize]);
         }
 
-        public UI.TimeDisplay createTimeButton(int runTime) {
-            UI.TimeDisplay time = new UI.TimeDisplay(logic.GenerateTimeStringFromSeconds(runTime));
+        public TimeDisplay createTimeButton(int runTime) {
+            TimeDisplay time = new TimeDisplay(logic.GenerateTimeStringFromSeconds(runTime));
             time.getStyle().setAlignment(CENTER);
             time.getAllStyles().setFgColor(0x752c29);
             time.getAllStyles().setBgTransparency(0);
@@ -362,12 +362,14 @@ public class UI extends Form {
             return time;
         }
 
-        private void toggleRunning(UI.TimeDisplay time) {
+        private void toggleRunning(TimeDisplay time) {
             if (time.timerRunning) {
+                deregisterAnimated(time);
                 time.stop(time);
                 logic.StopTask(taskName);
             }
             else {
+                registerAnimated(time);
                 time.start(time);
                 logic.StartTask(taskName);
             }
@@ -438,98 +440,6 @@ public class UI extends Form {
             }
             else {
                 return 0;
-            }
-        }
-    }
-
-
-
-    public class TimeDisplay extends Button {
-        int seconds;
-        int minutes;
-        int hours;
-        int days;
-        public String timeStamp;
-        int lastSecond;
-        boolean timerRunning;
-
-        public TimeDisplay(String runTime) {
-            seconds = 0;
-            minutes = 0;
-            hours = 0;
-            days = 0;
-            timeStamp = runTime;
-            Calendar curTime = Calendar.getInstance();
-            lastSecond = curTime.get(Calendar.SECOND);
-            timerRunning =  false;
-            setText(timeStamp);
-        }
-
-        public void start(Button time) {
-            timerRunning = true;
-            registerAnimated(time);
-        }
-
-        public void stop(Button time) {
-            timerRunning = false;
-            deregisterAnimated(time);
-        }
-
-        public boolean animate() {
-            if (timePassed()) {
-                secondPassed();
-                setText(timeStamp);
-                return true;
-            }
-
-            return false;
-        }
-
-        private boolean timePassed() {
-            Calendar curTime = Calendar.getInstance();
-            int  curSec = curTime.get(Calendar.SECOND);
-            if (curSec != lastSecond) {
-                lastSecond = curSec;
-                return true;
-            }
-            else {return false;}
-        }
-
-        private void secondPassed() {
-            seconds++;
-            if (seconds == 60) {
-                seconds = 0;
-                minutes++;
-            }
-            if (minutes == 60) {
-                minutes = 0;
-                hours++;
-            }
-            if (hours == 24) {
-                hours = 0;
-                days++;
-            }
-            setTimeString();
-        }
-
-        private void setTimeString() {
-            if (days > 0)
-            {
-                timeStamp = days + ":" + pad(hours);
-            }
-            else if (hours > 0) {
-                timeStamp = pad(hours) + ":" + pad(minutes);
-            }
-            else {
-                timeStamp = pad(minutes) + ":" + pad(seconds);
-            }
-        }
-
-        private String pad(int number) {
-            if (number < 10) {
-                return "0" + number;
-            } else {
-                return String.valueOf(number);
             }
         }
     }
