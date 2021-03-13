1aee5f0173594e0015ed280e754c93bafd55ab20
CG-DAVIS <60015986+CG-DAVIS@users.noreply.github.com>
2021-03-12 20:54:46 -0800

    Update TimeDisplay.java

--- TimeDisplay.java
+++ TimeDisplay.java
@@ -12,25 +12,28 @@ public class TimeDisplay extends Button {
         public String timeStamp;
         int lastSecond;
         boolean timerRunning;
+        String verbose;
+        String taskName;
 
-        public TimeDisplay(String runTime) {
-            seconds = 0;
-            minutes = 0;
-            hours = 0;
-            days = 0;
-            timeStamp = runTime;
+        public TimeDisplay(int timing, String tName) {
+            updateTimeStamp(timing);
+            setTimeString();
             Calendar curTime = Calendar.getInstance();
             lastSecond = curTime.get(Calendar.SECOND);
             timerRunning =  false;
             setText(timeStamp);
+            taskName = tName;
+            verboseString();
         }
 
-        public void start(Button time) {
+        public void start() {
             timerRunning = true;
+            getAllStyles().setFgColor(0x00CD00);
         }
 
         public void stop(Button time) {
             timerRunning = false;
+            getAllStyles().setFgColor(0x752c29);
         }
 
         public boolean animate() {
@@ -41,6 +44,21 @@ public class TimeDisplay extends Button {
             }
 
             return false;
+        }
+        
+        public String getVerbose() { return verbose; }
+
+        public void updateTimeStamp(int runTime) {
+            seconds = (runTime%3600)%60;
+            minutes = (runTime%3600)/60;
+            hours = (runTime/3600);
+            days = hours/24;
+            hours = hours-(days*24);
+
+            setTimeString();
+            Calendar curTime = Calendar.getInstance();
+            lastSecond = curTime.get(Calendar.SECOND);
+            setText(timeStamp);
         }
 
         private boolean timePassed() {
@@ -67,6 +85,7 @@ public class TimeDisplay extends Button {
                 hours = 0;
                 days++;
             }
+            verboseString();
             setTimeString();
         }
 
@@ -81,6 +100,14 @@ public class TimeDisplay extends Button {
             else {
                 timeStamp = pad(minutes) + ":" + pad(seconds);
             }
+        }
+        
+        private void verboseString() {
+            String sDays = "D:" + pad(days);
+            String sHours = " - H:" + pad(hours);
+            String sMins = " - M:" + pad(minutes);
+            String sSecs = " - S:" + pad(seconds);
+            verbose = sDays + sHours + sMins + sSecs;
         }
 
         private String pad(int number) {
